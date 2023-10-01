486f247ca113dcd116cbcb85cded91c939915502
ajitdhanukmandal <theajitmandal@gmail.com>
2023-10-01 20:31:42 +0545

    this is next commit

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
